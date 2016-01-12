b87c7ebf08563eeac52b3d0a6d654931efc96b88
Tyler Hawkins <thawkin3@byu.net>
2016-01-11 22:52:40 -0700

    DOM manipulation for requirement

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
+	
 	$('a').click(function(){
 	    $('html, body').animate({
 	        scrollTop: $( $(this).attr('href') ).offset().top
 	    }, 500);
 	    return false;
 	});
+
+	$("#myName").click(function(){
+		$(this).animate({
+		    fontSize: "+=20px"
+		}, 3000);
+		$(this).animate({
+		    fontSize: "-=20px"
+		}, 3000);
+	});
+
 });
