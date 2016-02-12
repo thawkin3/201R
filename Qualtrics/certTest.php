56ca2bbb80d8bf73ba7f88e640377d4c75411f3b
Tyler Hawkins <thawkin3@byu.net>
2016-02-11 22:25:49 -0700

    php json

--- Qualtrics/certTest.php
+++ Qualtrics/certTest.php
@@ -1,23 +1,51 @@
 <?php
 
 	$user = $_GET['user'];	
-/*
-	if ($user != "") {
-		echo $user;
-	} else {
-		echo "error";
+
+	switch ($user) {
+		case "QeiaOrgana@qcert.com":
+			$myJSON = '{"firstName": "Qeia", "lastName": "Organa", "email": "QeiaOrgana@qcert.com", "purchase": "Blaster Rifle", "discountItem1": "Silencer", "discountItem2": "Scope", "discountItem3": "Phaser", "discountItem4": "Holster", "discountPrice": "15%", "dealOfTheDay": "Double-Bladed Lightsaber"}';
+			break;
+		case "QukeSkywalker@qcert.com":
+			$myJSON = '{"firstName": "Quke", "lastName": "Skywalker", "email": "QukeSkywalker@qcert.com", "purchase": "Lightsaber", "discountItem1": "Belt", "discountItem2": "Belt Pouch", "discountItem3": "Belt Bundle", "discountItem4": "Cloaks", "discountPrice": "10%", "dealOfTheDay": "Double-Bladed Lightsaber"}';
+			break;
+		case "QarthVader@qcert.com":
+			$myJSON = '{"firstName": "Qarth", "lastName": "Vader", "email": "QarthVader@qcert.com", "purchase": "ATAT (Heavy Machinery)", "discountItem1": "Tool Kit", "discountItem2": "Junk Parts", "discountItem3": "Tow Cable Cutters", "discountItem4": "Anti-Ewok Sealant", "discountPrice": "0%", "dealOfTheDay": "Double-Bladed Lightsaber"}';
+			break;
+		case "QobaFett@qcert.com":
+			$myJSON = '{"firstName": "Qoba", "lastName": "Fett", "email": "QobaFett@qcert.com", "purchase": "Jet Pack", "discountItem1": "Fuel", "discountItem2": "Replacement Engine", "discountItem3": "Tool Kit", "discountItem4": "Decimator", "discountPrice": "15%", "dealOfTheDay": "Double-Bladed Lightsaber"}';
+			break;
+		case "QanSolo@qcert.com":
+			$myJSON = '{"firstName": "Qan", "lastName": "Solo", "email": "QanSolo@qcert.com", "purchase": "Blaster Rifle", "discountItem1": "Silencer", "discountItem2": "Scope", "discountItem3": "Phaser", "discountItem4": "Holster", "discountPrice": "15%", "dealOfTheDay": "Double-Bladed Lightsaber"}';
+			break;
+		case "Qobi-WanKenobi@qcert.com":
+			$myJSON = ;
+			break;
+		case "Q2D2@qcert.com":
+			$myJSON = ;
+			break;
+		case "Qoda@qcert.com":
+			$myJSON = ;
+			break;
+		case "QadmeAmidala@qcert.com":
+			$myJSON = ;
+			break;
+		case "QyloRen@qcert.com":
+			$myJSON = ;
+			break;
+		case "Qey@qcert.com":
+			$myJSON = ;
+			break;
+		case "QoeDameron@qcert.com":
+			$myJSON = ;
+			break;
+		case "Qinn@qcert.com":
+			$myJSON = ;
+			break;
+		default:
+			$myJSON = '{"Error Message": "incorrect email address"}';
 	}
-*/
 
-	//$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
-
-	//echo json_encode($arr);
-
-	//$myJSON = '{"QeiaOrgana@qcert.com":{"firstName":"Qeia","lastName":"Organa","email":"QeiaOrgana@qcert.com","purchase":"Blaster Rifle","discountItems":{"item1":"Silencer","item2":"Scope","item3":"Phaser","item4":"Holster"},"discountPrice":"15%","dealOfTheDay":"Double-Bladed Lightsaber"}';
-	$myJSON = '{ "firstName": "Qeia", "lastName": "Organa", "email": "QeiaOrgana@qcert.com", "purchase": "Blaster Rifle", "discountItem1": "Silencer", "discountItem2": "Scope", "discountItem3": "Phaser", "discountItem4": "Holster", "discountPrice": "15%", "dealOfTheDay": "Double-Bladed Lightsaber"}';
-	//$myJSON = '{"a":"apple", "b":"banana", "c":"cat"}';
-
-	#$myJSON = '{ "characters": { "QeiaOrgana@qcert.com": { "firstName": "Qeia", "lastName": "Organa", "email": "QeiaOrgana@qcert.com", "purchase": "Blaster Rifle", "discountItems": { "item1": "Silencer", "item2": "Scope", "item3": "Phaser", "item4": "Holster" }, "discountPrice": "15%", "dealOfTheDay": "Double-Bladed Lightsaber" }, "QukeSkywalker@qcert.com": { "firstName": "Quke", "lastName": "Skywalker", "email": "QukeSkywalker@qcert.com", "purchase": "Lightsaber", "discountItems": { "item1": "Belt", "item2": "Belt Pouch", "item3": "Belt Bundle", "item4": "Cloaks" }, "discountPrice": "10%", "dealOfTheDay": "Double-Bladed Lightsaber" }, "QarthVader@qcert.com": { "firstName": "Qarth", "lastName": "Vader", "email": "QarthVader@qcert.com", "purchase": "ATAT (Heavy Machinery)", "discountItems": { "item1": "Tool Kit", "item2": "Junk Parts", "item3": "Tow Cable Cutters", "item4": "Anti-Ewok Sealant" }, "discountPrice": "0%", "dealOfTheDay": "Double-Bladed Lightsaber" }, "QobaFett@qcert.com": { "firstName": "Qoba", "lastName": "Fett", "email": "QobaFett@qcert.com", "purchase": "Jet Pack", "discountItems": { "item1": "Fuel", "item2": "Replacement Engine", "item3": "Tool Kit", "item4": "Decimator" }, "discountPrice": "15%", "dealOfTheDay": "Double-Bladed Lightsaber" }, "QanSolo@qcert.com": { "firstName": "Qan", "lastName": "Solo", "email": "QanSolo@qcert.com", "purchase": "Blaster Rifle", "discountItems": { "item1": "Silencer", "item2": "Scope", "item3": "Phaser", "item4": "Holster" }, "discountPrice": "15%", "dealOfTheDay": "Double-Bladed Lightsaber" }, "Qobi-WanKenobi@qcert.com": { "firstName": "Qobi-Wan", "lastName": "Kenobi", "email": "Qobi-WanKenobi@qcert.com", "purchase": "Lightsaber", "discountItems": { "item1": "Belt", "item2": "Belt Pouch", "item3": "Belt Bundle", "item4": "Cloaks" }, "discountPrice": "10%", "dealOfTheDay": "Double-Bladed Lightsaber" }, "Q2D2@qcert.com": { "firstName": "Q2", "lastName": "D2", "email": "Q2D2@qcert.com", "purchase": "Motivator (Heavy Machinery)", "discountItems": { "item1": "Tool Kit", "item2": "Junk Parts", "item3": "Tow Cable Cutters", "item4": "Anti-Ewok Sealant" }, "discountPrice": "0%", "dealOfTheDay": "Double-Bladed Lightsaber" }, "Qoda@qcert.com": { "firstName": "Qoda", "lastName": "", "email": "Qoda@qcert.com", "purchase": "Flashlight (supplies)", "discountItems": { "item1": "Backpack", "item2": "Batteries", "item3": "Food Packs", "item4": "First Aid Kit" }, "discountPrice": "30%", "dealOfTheDay": "Double-Bladed Lightsaber" }, "QadmeAmidala@qcert.com": { "firstName": "Qadme", "lastName": "Amidala", "email": "QadmeAmidala@qcert.com", "purchase": "Blaster Rifle", "discountItems": { "item1": "Silencer", "item2": "Scope", "item3": "Phaser", "item4": "Holster" }, "discountPrice": "15%", "dealOfTheDay": "Double-Bladed Lightsaber" }, "QyloRen@qcert.com": { "firstName": "Qylo", "lastName": "Red", "email": "QyloRen@qcert.com", "purchase": "Lightsaber", "discountItems": { "item1": "Belt", "item2": "Belt Pouch", "item3": "Belt Bundle", "item4": "Cloaks" }, "discountPrice": "10%", "dealOfTheDay": "Double-Bladed Lightsaber" }, "Qey@qcert.com": { "firstName": "Qey", "lastName": "", "email": "Qey@qcert.com", "purchase": "Dehydrated Meal Pack (supplies)", "discountItems": { "item1": "Backpack", "item2": "Batteries", "item3": "Food Packs", "item4": "First Aid Kit" }, "discountPrice": "30%", "dealOfTheDay": "Double-Bladed Lightsaber" }, "QoeDameron@qcert.com": { "firstName": "Qoe", "lastName": "Dameron", "email": "QoeDameron@qcert.com", "purchase": "X-Wing (Heavy Machinery)", "discountItems": { "item1": "Tool Kit", "item2": "Junk Parts", "item3": "Tow Cable Cutters", "item4": "Anti-Ewok Sealant" }, "discountPrice": "0%", "dealOfTheDay": "Double-Bladed Lightsaber" }, "Qinn@qcert.com": { "firstName": "Qinn", "lastName": "", "email": "Qinn@qcert.com", "purchase": "Leather Jacket (clothes)", "discountItems": { "item1": "Robes", "item2": "Boots", "item3": "Helmet", "item4": "Armour" }, "discountPrice": "25%", "dealOfTheDay": "Double-Bladed Lightsaber" } } }';
 	echo json_encode(json_decode($myJSON));
 
 ?>
