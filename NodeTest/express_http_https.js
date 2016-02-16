aad60ec2a61ee6c88fd71a308935994eca0aa9ea
Tyler Hawkins <thawkin3@byu.net>
2016-02-15 22:05:21 -0700

    path changes

--- NodeTest/express_http_https.js
+++ NodeTest/express_http_https.js
@@ -5,8 +5,8 @@ var fs = require('fs');
 var app = express();
 var options = {
     host: '127.0.0.1',
-    key: fs.readFileSync('ssl/server.key'),
-    cert: fs.readFileSync('ssl/server.crt')
+    key: fs.readFileSync('../ssl/server.key'),
+    cert: fs.readFileSync('../ssl/server.crt')
   };
 http.createServer(app).listen(80);
 https.createServer(options, app).listen(443);
