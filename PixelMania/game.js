64b0ec9fa53bdb799124e0bc89b506fcb5a2dfb1
Tyler Hawkins <thawkin3@byu.net>
2016-02-29 20:57:32 -0700

    trying port 4000 and a new server

--- PixelMania/game.js
+++ PixelMania/game.js
@@ -6,6 +6,47 @@ var util = require("util"),					// Utility resources (logging, object inspection
 	Player = require("./Player").Player;	// Player class
 
 
+/* NEW STUFF */
+var http = require("http");
+var url = require('url');
+var fs = require('fs');
+
+var server = http.createServer(function(request, response){
+    var path = url.parse(request.url).pathname;
+
+    switch(path){
+        case '/':
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write('hello world');
+            response.end();
+            break;
+        case '/socket.html':
+            fs.readFile(__dirname + path, function(error, data){
+                if (error){
+                    response.writeHead(404);
+                    response.write("opps this doesn't exist - 404");
+                    response.end();
+                }
+                else{
+                    response.writeHead(200, {"Content-Type": "text/html"});
+                    response.write(data, "utf8");
+                    response.end();
+                }
+            });
+            break;
+        default:
+            response.writeHead(404);
+            response.write("opps this doesn't exist - 404");
+            response.end();
+            break;
+    }
+});
+
+server.listen(4000);
+
+io.listen(server);
+
+
 /**************************************************
 ** GAME VARIABLES
 **************************************************/
@@ -21,7 +62,7 @@ function init() {
 	players = [];
 
 	// Set up Socket.IO to listen on port 80
-	socket = io.listen(80);
+	socket = io.listen(4000);
 
 	// Configure Socket.IO
 	socket.configure(function() {
