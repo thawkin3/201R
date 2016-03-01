813bd7bf5fac5946f34423fc8a5c45661fa2ddc2
thawkin3 <thawkin3@byu.net>
2016-03-01 17:30:12 +0000

    pokemon activity

---
+++ NodeTest/poki/public/javascripts/app.js
@@ -0,0 +1,46 @@
+var app = window.angular.module('app', [])
+
+app.factory('pokemonFetcher', pokemonFetcher)
+app.controller('mainCtrl', mainCtrl)
+
+function pokemonFetcher ($http) {
+
+  var API_ROOT = 'pokemon'
+  return {
+    get: function () {
+      return $http
+        .get(API_ROOT)
+        .then(function (resp) {
+          return resp.data
+        })
+    },
+    tryit: function() {
+      var politics = "/politics";
+
+      return $http
+        .get(politics)
+        .then(function (resp) {
+          console.log("Get Worked");
+          console.log(resp.data);
+          return resp.data
+        })
+    }
+  }
+
+}
+
+function mainCtrl ($scope, pokemonFetcher) {
+
+  $scope.pokemon = []
+
+  pokemonFetcher.get()
+    .then(function (data) {
+      $scope.pokemon = data
+    })
+
+  pokemonFetcher.tryit()
+    .then(function (data) {
+      console.log("tryit");
+      console.log(data);
+    })
+}
