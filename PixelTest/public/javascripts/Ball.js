648158ca96a4b1a6d8b079970f2322f2fe62a57e
Tyler Hawkins <thawkin3@byu.net>
2016-03-04 22:51:36 -0700

    adding Ball class

--- PixelTest/public/javascripts/Ball.js
+++ PixelTest/public/javascripts/Ball.js
@@ -73,7 +73,7 @@ var Ball = function(startX, startY) {
 	var draw = function(ctx) {
 		ctx.beginPath();  // Starts a new shape.
 	    ctx.fillStyle = ball.fillColor;  // Sets the fill color.
-	    ctx.arc(x, y, size, 0, Math.PI*2);  // Draws a full 2 pi arc (circle).
+	    ctx.arc(x, y, radius, 0, Math.PI*2);  // Draws a full 2 pi arc (circle).
 	    ctx.lineWidth = 1;  // Small line width.
 	    ctx.fill();  // Fills the circle with the specified fill color.
 	    ctx.stroke();  // Actually draws the circle on the canvas now.
