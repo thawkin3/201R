a5ef96f86eb78dd77c7d54c5520f04a6b944daf9
Tyler Hawkins <thawkin3@byu.net>
2016-03-06 16:14:00 -0700

    rest service

--- CorporateBS/routes/index.js
+++ CorporateBS/routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.sendFile('index.html', { root:  'public' });
 });
 
+/* REST service to generate some BS */
+router.get('/getwords',function(req, res, next) {
+	console.log("In getwords route");
+	
+	fs.readFile(__dirname + '/texts/adverbs.txt', function(err, data) {
+        
+        if (err) throw err;
+
+        var jsonResult = [];
+        var adverbs = data.toString().split("\n");
+        while (jsonResult.length < 3) {
+        	var index = Math.floor * (Math.random() * adverbs.length);
+        	if jsonResult.indexOf(adverbs[index] == -1) {
+        		jsonResult.push(adverbs[index]);
+        	}
+        }
+
+        console.log(jsonResult);
+
+        res.status(200).json(jsonResult);
+
+    });
+});
+
 module.exports = router;
