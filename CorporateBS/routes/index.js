c6f7365bd84f6ab88d1585231ee4170212be33f3
Tyler Hawkins <thawkin3@byu.net>
2016-03-06 16:28:33 -0700

    rest service

--- CorporateBS/routes/index.js
+++ CorporateBS/routes/index.js
@@ -33,7 +33,72 @@ router.get('/getwords',function(req, res, next) {
 
         console.log(jsonResult);
 
-        res.status(200).json(jsonResult);
+        if (jsonResult.adverbs.length == 3 && jsonResult.verbs.length == 3 && jsonResult.adjectives.length == 3 && jsonResult.nouns.length == 3) {
+        	res.status(200).json(jsonResult);
+        }
+
+    });
+
+    fs.readFile(__dirname + '/texts/verbs.txt', function(err, data) {
+        
+        if (err) throw err;
+
+        var verbs = data.toString().split("\n");
+        console.log(verbs);
+        while (jsonResult.verbs.length < 3) {
+        	var index = Math.floor(Math.random() * verbs.length);
+        	if (jsonResult.verbs.indexOf(verbs[index] == -1)) {
+        		jsonResult.verbs.push(verbs[index]);
+        	}
+        }
+
+        console.log(jsonResult);
+
+        if (jsonResult.adverbs.length == 3 && jsonResult.verbs.length == 3 && jsonResult.adjectives.length == 3 && jsonResult.nouns.length == 3) {
+        	res.status(200).json(jsonResult);
+        }
+
+    });
+
+    fs.readFile(__dirname + '/texts/adjectives.txt', function(err, data) {
+        
+        if (err) throw err;
+
+        var adjectives = data.toString().split("\n");
+        console.log(adjectives);
+        while (jsonResult.adjectives.length < 3) {
+        	var index = Math.floor(Math.random() * adjectives.length);
+        	if (jsonResult.adjectives.indexOf(adjectives[index] == -1)) {
+        		jsonResult.adjectives.push(adjectives[index]);
+        	}
+        }
+
+        console.log(jsonResult);
+
+        if (jsonResult.adverbs.length == 3 && jsonResult.verbs.length == 3 && jsonResult.adjectives.length == 3 && jsonResult.nouns.length == 3) {
+        	res.status(200).json(jsonResult);
+        }
+
+    });
+
+    fs.readFile(__dirname + '/texts/nouns.txt', function(err, data) {
+        
+        if (err) throw err;
+
+        var nouns = data.toString().split("\n");
+        console.log(nouns);
+        while (jsonResult.nouns.length < 3) {
+        	var index = Math.floor(Math.random() * nouns.length);
+        	if (jsonResult.nouns.indexOf(nouns[index] == -1)) {
+        		jsonResult.nouns.push(nouns[index]);
+        	}
+        }
+
+        console.log(jsonResult);
+
+        if (jsonResult.adverbs.length == 3 && jsonResult.verbs.length == 3 && jsonResult.adjectives.length == 3 && jsonResult.nouns.length == 3) {
+        	res.status(200).json(jsonResult);
+        }
 
     });
 });
