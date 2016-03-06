9f3063e8daf24764c6422a2eb73f7a2c354bfe1c
Tyler Hawkins <thawkin3@byu.net>
2016-03-05 23:15:32 -0700

    comments styles

--- comments/public/javascripts/comments.js
+++ comments/public/javascripts/comments.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $("#serialize").click(function(){
         var myobj = {Name:$("#Name").val(),Comment:$("#Comment").val()};
         jobj = JSON.stringify(myobj);
-        $("#json").text("Your JSON stringified comment:" + jobj);
+        $("#json").text("Your JSON stringified comment:" + jobj.toString());
     
     	var url = "comment";
 		$.ajax({
