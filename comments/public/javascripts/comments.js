c867d3a99c1abd838e3926ba0ea88f53ca2c4696
Tyler Hawkins <thawkin3@byu.net>
2016-03-05 22:00:20 -0700

    comments

--- comments/public/javascripts/comments.js
+++ comments/public/javascripts/comments.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
+    
     $("#serialize").click(function(){
         var myobj = {Name:$("#Name").val(),Comment:$("#Comment").val()};
         jobj = JSON.stringify(myobj);
         $("#json").text(jobj);
     
-
     	var url = "comment";
 		$.ajax({
   			url:url,
@@ -19,16 +19,16 @@ $(document).ready(function(){
 	});
 
     $("#getThem").click(function() {
-      $.getJSON('comments', function(data) {
-        console.log(data);
-        var everything = "<ul>";
-        for(var comment in data) {
-          com = data[comment];
-          everything += "<li>Name: " + com.Name + " -- Comment: " + com.Comment + "</li>";
-        }
-        everything += "</ul>";
-        $("#comments").html(everything);
-      })
-    })
+        $.getJSON('comments', function(data) {
+            console.log(data);
+            var everything = "<ul>";
+            for(var comment in data) {
+              com = data[comment];
+              everything += "<li>Name: " + com.Name + " -- Comment: " + com.Comment + "</li>";
+            }
+            everything += "</ul>";
+            $("#comments").html(everything);
+        });
+    });
 
 });
