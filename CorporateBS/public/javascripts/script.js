58721a184c22542ba15b80863c23113525ffd129
Tyler Hawkins <thawkin3@byu.net>
2016-03-06 16:49:07 -0700

    ajax

--- CorporateBS/public/javascripts/script.js
+++ CorporateBS/public/javascripts/script.js
@@ -1,22 +1,13 @@
 $(document).ready(function(){
 
 	var words;
+	var name;
 
 	$("#firstPageForm").submit(function(){
 		$("#firstPage").hide();
 		$("#secondPage").show();
-
-		$.getJSON("../../getwords", function(data) {
-	    	words = data;
-	    	console.log(words);
-	    })
-	    .done(function() { 
-	    	console.log('generated your BS!'); 
-	    })
-	    .fail(function(jqXHR, textStatus, errorThrown) { 
-	    	console.log('failed to generate BS!' + textStatus); 
-	    	console.log("incoming " + jqXHR.responseText);
-	    });
+		name = $("#name").val();
+		generateBS();
 	});
 
 	$("#secondPageForm").submit(function(){
@@ -32,6 +23,7 @@ $(document).ready(function(){
 	$("#fourthPageForm").submit(function(){
 		$("#fourthPage").hide();
 		$("#secondPage").show();
+		generateBS();
 	});
 
 	$(window).resize(function() {
@@ -40,6 +32,24 @@ $(document).ready(function(){
 		} else {
 			$("#mainContent").show();
 		}
+	});
+
+	function generateBS() {
+		$.getJSON("../../getwords", function(data) {
+	    	words = data;
+	    	console.log(words);
+	    })
+	    .done(function() { 
+	    	$("#text1page2").html("Hey " + name + ", what do you think about " + words.nouns[0] + "?");
+	    	$("#text2page2").html("I think we should " + words.adverbs[0] + " " + words.verbs[0] + " " words.nouns[0] + " by " words.adverbs[1] + " " + words.verbs[1] + " " words.nouns[1] + ".");
+	    	$("#text1page3").html("Of course! I knew I liked you " + name + ". With that idea, we'll be sure to " + words.adverbs[2] + " " + words.verbs[2] + " " words.nouns[2] + " in no time.");
+	    	$("#text2page3").html("Whatever you say, boss.");
+	    	console.log('generated your BS!'); 
+	    })
+	    .fail(function(jqXHR, textStatus, errorThrown) { 
+	    	console.log('failed to generate BS!' + textStatus); 
+	    	console.log("incoming " + jqXHR.responseText);
+	    });
 	});
 
 });
