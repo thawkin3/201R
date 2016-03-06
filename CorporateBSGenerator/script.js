639964fa2077d0cb956de6715205b57787e4c0c6
Tyler Hawkins <thawkin3@byu.net>
2016-03-05 20:52:57 -0700

    bs

---
+++ CorporateBSGenerator/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+	$("#firstPageForm").submit(function(){
+		$("#firstPage").hide();
+		$("#secondPage").show();
+	});
+
+	$("#secondPageForm").submit(function(){
+		$("#secondPage").hide();
+		$("#thirdPage").show();
+	});
+
+	$("#thirdPageForm").submit(function(){
+		$("#thirdPage").hide();
+		$("#fourthPage").show();
+	});
+
+	$("#fourthPageForm").submit(function(){
+		$("#fourthPage").hide();
+		$("#secondPage").show();
+	});
+
+});
