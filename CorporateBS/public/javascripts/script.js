fbfdd127aee347a54d5658ed35c881a466c7155e
Tyler Hawkins <thawkin3@byu.net>
2016-03-06 17:01:27 -0700

    ajax

--- CorporateBS/public/javascripts/script.js
+++ CorporateBS/public/javascripts/script.js
@@ -7,6 +7,9 @@ $(document).ready(function(){
 		$("#firstPage").hide();
 		$("#secondPage").show();
 		name = $("#name").val();
+		if (name == "") {
+			name == "valued employee";
+		}
 		generateBS();
 	});
 
@@ -40,7 +43,7 @@ $(document).ready(function(){
 	    	console.log(words);
 	    })
 	    .done(function() { 
-	    	$("#text1page2").html("Hey " + name + ", what do you think about " + words.nouns[0] + "?");
+	    	$("#text1page2").html("Hey " + name + ", what do you think about " + words.nouns[0] + "? We're looking for some new ideas and need all the help we can get.");
 	    	$("#text2page2").html("I think we should " + words.adverbs[0] + " " + words.verbs[0] + " " + words.nouns[0] + " so that we can " + words.adverbs[1] + " " + words.verbs[1] + " " + words.nouns[1] + ".");
 	    	$("#text1page3").html("Of course! I knew I liked you " + name + ". With that idea, we'll be sure to " + words.adverbs[2] + " " + words.verbs[2] + " " + words.nouns[2] + " and " + words.adverbs[3] + " " + words.verbs[3] + " " + words.nouns[3] + " in no time.");
 	    	$("#text2page3").html("Whatever you say, boss.");
