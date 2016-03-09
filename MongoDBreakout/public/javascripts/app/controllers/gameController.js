352ea5039a5f2ec870eae169d73df61d34832911
Tyler Hawkins <thawkin3@byu.net>
2016-03-08 22:40:54 -0700

    breakout game

--- MongoDBreakout/public/javascripts/app/controllers/gameController.js
+++ MongoDBreakout/public/javascripts/app/controllers/gameController.js
@@ -62,10 +62,10 @@
 		    // If the ball has hit the top or bottom, bounce it.
 		    if ((ball_y + (ball_size/2) >= canvas.height) || (ball_y - (ball_size/2) <= 0)) { 
 		    	ball_dy = -ball_dy; 
-		    	console.log(ctx.getImageData(ball_x, ball_y - (ball_size/2), 1, 1).data[0]);
+		    	console.log(ctx.getImageData(ball_x, ball_y - ball_size, 1, 1).data[0]);
 		    }
 
-		    if (ctx.getImageData(ball_x, ball_y - (ball_size/2), 1, 1).data[0] == 241 && hitReset) {
+		    if (ctx.getImageData(ball_x, ball_y - ball_size, 1, 1).data[0] == 241 && hitReset) {
 		    	hitReset = false;
 		    	ball_dy = -ball_dy;
 		    	setTimeout(function(){
