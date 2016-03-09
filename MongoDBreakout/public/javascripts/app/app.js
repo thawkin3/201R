8efbb24b22b516d841258803fc04a8a5b6d82105
Tyler Hawkins <thawkin3@byu.net>
2016-03-08 19:39:49 -0700

    fixing path issues

--- MongoDBreakout/public/javascripts/app/app.js
+++ MongoDBreakout/public/javascripts/app/app.js
@@ -4,15 +4,15 @@ app.config(function ($routeProvider){
 	$routeProvider
 		.when('/', {
 			controller: 'authenticateController',
-			templateUrl:'public/javascripts/app/views/authenticateView.html'
+			templateUrl:'javascripts/app/views/authenticateView.html'
 		})
 		.when('/game', {
 			controller: 'gameController',
-			templateUrl:'public/javascripts/app/views/gameView.html'
+			templateUrl:'javascripts/app/views/gameView.html'
 		})
 		.when('/highscores', {
 			controller: 'highscoresController',
-			templateUrl:'public/javascripts/app/views/highscoresView.html'
+			templateUrl:'javascripts/app/views/highscoresView.html'
 		})
 		.otherwise({ 
 			redirectTo: '/' 
