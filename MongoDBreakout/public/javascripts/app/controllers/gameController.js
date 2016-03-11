9fdc5c1b3598d2c792b3b376b042c141594cfeb6
Tyler Hawkins <thawkin3@byu.net>
2016-03-10 22:39:47 -0700

    bounce

--- MongoDBreakout/public/javascripts/app/controllers/gameController.js
+++ MongoDBreakout/public/javascripts/app/controllers/gameController.js
@@ -91,12 +91,14 @@
 		    if (ctx.getImageData(ball_x, ball_y - 3 - ball_size/2, 1, 1).data[0] == 241) {
 		    	ball_dy = -ball_dy;
 		    	$scope.brickArray = $scope.brickArray.filter(filterBricksHitFromBelow);
+		    	$scope.score = 32 - $scope.brickArray.length;
 		    }
 
 		    // If the ball has hit a brick from above, bounce it.
 		    if (ctx.getImageData(ball_x, ball_y + 1 + ball_size/2, 1, 1).data[0] == 241) {
 		    	ball_dy = -ball_dy;
 		    	$scope.brickArray = $scope.brickArray.filter(filterBricksHitFromAbove);
+		    	$scope.score = 32 - $scope.brickArray.length;
 		    }
 
 			// Move the ball to its new position.
