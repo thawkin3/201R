5c1ee0065cc8638d1d1d4a72f6263f1af5768165
Tyler Hawkins <thawkin3@byu.net>
2016-03-10 21:21:54 -0700

    bounce

--- MongoDBreakout/public/javascripts/app/controllers/gameController.js
+++ MongoDBreakout/public/javascripts/app/controllers/gameController.js
@@ -87,7 +87,7 @@
 		    	console.log(ctx.getImageData(ball_x, ball_y + (ball_size/2), 1, 1).data[0]);
 		    	hitReset = false;
 		    	ball_dy = -ball_dy;
-		    	// ball_y -= 10;
+		    	ball_y -= 10;
 		    	setTimeout(function(){
 		    		hitReset = true;
 		    	}, 1000);
