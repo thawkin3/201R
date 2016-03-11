043ff5299bf79fe15db3d2e3d2e8fcc6f1735ccc
Tyler Hawkins <thawkin3@byu.net>
2016-03-10 22:17:36 -0700

    bounce

--- MongoDBreakout/public/javascripts/app/controllers/gameController.js
+++ MongoDBreakout/public/javascripts/app/controllers/gameController.js
@@ -158,7 +158,7 @@
 		};
 
 		var filterBricks = function(brick) {
-			return ((ball_y - brick.t > 25) && (ball_x - brick.l < 10 || ball_x - brick.l > 30));
+			return ((ball_y - brick.t > 25) && (ball_x - brick.l < -40 || ball_x - brick.l > 40));
 		}
 
 		// Start the game
