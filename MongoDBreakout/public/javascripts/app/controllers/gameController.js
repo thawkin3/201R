d5ba0adcdcd7398ee9a3e32618adfb29fe5114b1
Tyler Hawkins <thawkin3@byu.net>
2016-03-10 21:02:55 -0700

    bounce

--- MongoDBreakout/public/javascripts/app/controllers/gameController.js
+++ MongoDBreakout/public/javascripts/app/controllers/gameController.js
@@ -83,11 +83,11 @@
 
 		    // If the ball has hit the paddle, bounce it.
 		    if (ctx.getImageData(ball_x, ball_y + ball_size/2, 1, 1).data[0] == 241 && hitReset) {
-		    	hitReset = false;
+		    	// hitReset = false;
 		    	ball_dy = -ball_dy;
-		    	setTimeout(function(){
-		    		hitReset = true;
-		    	}, 500);
+		    	// setTimeout(function(){
+		    	// 	hitReset = true;
+		    	// }, 500);
 		    }
 
 		    // If the ball has hit a brick from below, bounce it.
