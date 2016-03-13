eff72ed607fee887aa791a4909a3a80988756b13
Tyler Hawkins <thawkin3@byu.net>
2016-03-13 17:50:03 -0600

    timestamp

--- TimeStamp/routes/index.js
+++ TimeStamp/routes/index.js
@@ -10,33 +10,22 @@ router.get('/', function(req, res, next) {
 
 
 /* TimeStamp MicroService */
-/*
+
 router.get('/gettime',function(req, res, next) {
 	console.log("In gettime route");
-	
-	fs.readFile(__dirname + '/cities.txt', function(err, data) {
-        
-        if (err) throw err;
-        
-        var myRe = new RegExp("^" + req.query.q);
-        console.log(myRe);
-        var jsonResult = [];
-        var cities = data.toString().split("\n");
-        
-        for (var i = 0; i < cities.length; i++) {
-        	var result = cities[i].search(myRe);
-          	if (result != -1) {
-            	console.log(cities[i]);
-            	jsonResult.push({city:cities[i]});
-          	}
-        }
+	console.log(req.query.q);
 
-        console.log(jsonResult);
+    var jsonResult = {
+    	unix: null,
+    	normal: null
+    };
 
-        res.status(200).json(jsonResult);
+    console.log(jsonResult);
+
+    res.status(200).json(jsonResult);
 
     });
 });
-*/
+
 
 module.exports = router;
