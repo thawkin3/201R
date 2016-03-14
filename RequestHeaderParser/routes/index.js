419f44b44b25693da1cb2da6ab2731a24ac897b6
Tyler Hawkins <thawkin3@byu.net>
2016-03-13 20:22:30 -0600

    rhp

--- RequestHeaderParser/routes/index.js
+++ RequestHeaderParser/routes/index.js
@@ -9,14 +9,13 @@ router.get('/', function(req, res, next) {
     console.log(req.connection);
     console.log(req.connection.remoteAddress);
     console.log(req.socket.remoteAddress);
-    console.log(req.connection.socket.remoteAddress);
     var jsonResult = {
     	ip: null,
     	lang: null,
     	useragent: null
     }
 
-    jsonResult.ip = req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+    jsonResult.ip = req.connection.remoteAddress || req.socket.remoteAddress;
     jsonResult["lang"] = req.acceptedLanguages;
     jsonResult.useragent = req.headers['user-agent'];
 
