d63ccc9194f0822faae031fe755648abda8c479e
Tyler Hawkins <thawkin3@byu.net>
2016-03-13 18:51:42 -0600

    timestamp

--- TimeStamp/public/javascripts/script.js
+++ TimeStamp/public/javascripts/script.js
@@ -1,1 +1,23 @@
 console.log("script.js loaded");
+
+$(document).ready(function(){
+
+	$("dateForm").submit(function(){
+
+		var str = $("#theDate").val();
+
+		$.getJSON("../gettime?q="+str, function(data) {
+	    	$("#unix").html(data.unix);
+	    	$("normal").html(data.normal);
+	    })
+	    .done(function() { 
+	    	console.log('getJSON request succeeded!'); 
+	    })
+	    .fail(function(jqXHR, textStatus, errorThrown) { 
+	    	console.log('getJSON request failed!' + textStatus); 
+	    	console.log("incoming " + jqXHR.responseText);
+	    });
+	    
+	});
+
+});
