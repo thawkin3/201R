6dbdccb30c3975b664cf2110fa006312d7fb166a
Tyler Hawkins <thawkin3@byu.net>
2016-03-13 20:02:56 -0600

    rhp

--- RequestHeaderParser/routes/index.js
+++ RequestHeaderParser/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+    console.log("on the home page");
+  	res.status(200).json("hey":"there");
 });
 
 module.exports = router;
