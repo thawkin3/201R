b1129d9177af3feb5a0cae5c675a41418c333c19
Tyler Hawkins <thawkin3@byu.net>
2016-03-14 20:31:33 -0600

    breakout

--- MongoDBreakout/public/javascripts/app/controllers/authenticateController.js
+++ MongoDBreakout/public/javascripts/app/controllers/authenticateController.js
@@ -2,6 +2,39 @@
 
 	var authenticateController = function ($scope, $routeParams, $rootScope) {
 
+		$(document).ready(function(){
+
+		    $("#authetication").submit(function(){
+		        var theEmail = $("#inputEmail1").val();
+		        var theName = $("#inputName1").val();
+		        var thePassword = $("#inputPassword1").val();
+
+		        var myobj = {Name: theName, Email: theEmail, Password: thePassword};
+		        jobj = JSON.stringify(myobj);
+		        $("#json").text("Your JSON stringified user: " + jobj);
+		    
+		    	var url = "user";
+				$.ajax({
+		  			url:url,
+		  			type: "POST",
+		  			data: jobj,
+		  			contentType: "application/json; charset=utf-8",
+		  			success: function(data,textStatus) {
+		      				$("#done").html("Adding your user: " + textStatus + "!");
+		  			}
+				});
+
+		        $("#inputEmail1").val("");
+		        $("#inputName1").val("");
+		        $("#inputPassword1").val("");
+
+			});
+
+
+		});
+
+
+
 	};
 
 	authenticateController.$inject = ['$scope', '$routeParams', '$rootScope'];
--- MongoDBreakout/routes/index.js
+++ MongoDBreakout/routes/index.js
@@ -7,3 +7,51 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+
+
+/* Set up mongoose in order to connect to mongo database */
+var mongoose = require('mongoose'); //Adds mongoose as a usable dependency
+
+mongoose.connect('mongodb://localhost/breakoutDB'); //Connects to a mongo database called "commentDB"
+
+var userSchema = mongoose.Schema({ //Defines the Schema for this database
+  Name: String,
+  Email: String,
+  Password: String
+});
+
+var user = mongoose.model('User', userSchema); //Makes an object from that schema as a model
+
+var db = mongoose.connection; //Saves the connection as a variable to use
+db.on('error', console.error.bind(console, 'connection error:')); //Checks for connection errors
+db.once('open', function() { //Lets us know when we're connected
+  console.log('Connected');
+});
+
+/* POST a user */
+router.post('/user', function(req, res, next) {
+  console.log("POST user route"); //[1]
+  console.log(req.body); //[2]
+  var newUser = new User(req.body); //[3]
+  newUser.save(function(err, post) { //[4]
+    if (err) return console.error(err);
+    console.log(post);
+    res.sendStatus(200);
+  });
+});
+
+/* GET comments from database */
+router.get('/user', function(req, res, next) {
+  console.log("In the GET route");
+  Comment.find(function(err,commentList) { //Calls the find() method on your database
+    if (err) return console.error(err); //If there's an error, print it out
+    else {
+      console.log(commentList); //Otherwise console log the comments you found
+      res.json(commentList); //Then send them
+    }
+  })
+});
+
+module.exports = router;
