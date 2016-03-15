55c848ac48d17397f0201f124a7d9b41bb47d264
Tyler Hawkins <thawkin3@byu.net>
2016-03-14 22:04:29 -0600

    breakout

--- MongoDBreakout/routes/index.js
+++ MongoDBreakout/routes/index.js
@@ -53,7 +53,7 @@ router.post('/getuser', function(req, res, next) {
   console.log("POST getuser route"); //[1]
   console.log(req.body); //[2]
 
-  User.findOne({ Email: req.body.Email, Password: req.body.Password }, function(err, user) {
+  User.find({ Email: req.body.Email, Password: req.body.Password }, function(err, user) {
 	  console.log(user);
 	  if (user !== null) {
 		    console.log("found you!");
