a9f1c574e9c7463eea56434694d78bfdd7230f3e
Tyler Hawkins <thawkin3@byu.net>
2016-03-17 20:01:46 -0600

    scores

--- MongoDBreakout/public/javascripts/app/controllers/gameController.js
+++ MongoDBreakout/public/javascripts/app/controllers/gameController.js
@@ -269,7 +269,7 @@
 		    }
 
 		    // If the ball has hit the canvas's bottom wall, game over.
-		    if (ctx.getImageData(ball_x, ball_y + 15 + ball_size/2, 1, 1).data[0] == 119) {
+		    if (ctx.getImageData(ball_x, ball_y + 5 + ball_size/2, 1, 1).data[0] == 119) {
 		    //if (ball_y + (ball_size/2) >= canvas.height) {
 		    	console.log(ball_y + (ball_size/2));
 		    	console.log("game over");
