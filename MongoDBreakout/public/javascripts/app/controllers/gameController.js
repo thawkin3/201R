13aaa55ffe42f6e04503a50021bb52e01e02cb45
Tyler Hawkins <thawkin3@byu.net>
2016-03-20 13:41:43 -0600

    fixing ball location

--- MongoDBreakout/public/javascripts/app/controllers/gameController.js
+++ MongoDBreakout/public/javascripts/app/controllers/gameController.js
@@ -94,10 +94,11 @@
 		// Locator function in a loop
 		$scope.mainLoop = function() {
 		
-			console.log("ball_y: " + ball_y);
-			console.log("ball_prev_y: " + ball_prev_y);
 			ball_y_tracker.push(ball_y);
 			ball_y_tracker.push(ball_prev_y);
+			console.log("ball_y: " + ball_y);
+			console.log("ball_prev_y: " + ball_prev_y);
+
 			// console.log("ball_prev_prev_y: " + ball_prev_prev_y);
 			// if (ball_y > 1000 && ball_prev_y > 1000 && ball_prev_prev_y > 1000) {
 			if (ball_y_tracker[ball_y_tracker.length - 1] > 1000 && ball_y_tracker[ball_y_tracker.length - 2] > 1000 && ball_y_tracker[ball_y_tracker.length - 3] > 1000) {
