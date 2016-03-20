57b28ed1348ee23a54c9d7d6197d7f49a00ce30d
Tyler Hawkins <thawkin3@byu.net>
2016-03-20 13:35:52 -0600

    fixing ball location

--- MongoDBreakout/public/javascripts/app/controllers/gameController.js
+++ MongoDBreakout/public/javascripts/app/controllers/gameController.js
@@ -98,7 +98,7 @@
 			console.log("ball_prev_y: " + ball_prev_y);
 			console.log("ball_prev_prev_y: " + ball_prev_prev_y);
 			// if (ball_y > 1000 && ball_prev_y > 1000 && ball_prev_prev_y > 1000) {
-			if (ball_y_tracker[ball_y_tracker.length - 1] > 1000 && ball_y_tracker[ball_y_tracker.length - 2] > 1000) {
+			if (ball_y_tracker[ball_y_tracker.length - 1] > 1000 && ball_y_tracker[ball_y_tracker.length - 2] > 1000 && ball_y_tracker[ball_y_tracker.length - 3] > 1000) {
 				console.log(ball_y_tracker);
 				alert("greater than 1000");
 				console.log("game over");
@@ -151,10 +151,11 @@
 
 		// Update ball position
 		$scope.update = function() {
-			ball_y_tracker.push(ball_y);
+			
 			ball_prev_prev_y = ball_prev_y;
 			ball_prev_y = ball_y;
-
+			ball_y_tracker.push(ball_y);
+			ball_y_tracker.push(ball_prev_y);
 
 			// Move the paddle, left key takes priority over right
 			if (left && paddle_x > 0 + paddle_width/2) {
