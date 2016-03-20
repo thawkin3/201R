bf811b07d797a0eab167ae07bc65580f82a0deb0
Tyler Hawkins <thawkin3@byu.net>
2016-03-19 23:30:12 -0600

    fixing ball location

--- MongoDBreakout/public/javascripts/app/controllers/gameController.js
+++ MongoDBreakout/public/javascripts/app/controllers/gameController.js
@@ -270,6 +270,11 @@
 		    	ball_dy = -ball_dy; 
 		    }
 
+		    // If the ball has hit the bottom, bounce it.
+		    if (ball_y + (ball_size/2) >= canvas.height) { 
+		    	ball_dy = -ball_dy; 
+		    }
+
 		    // If the ball has hit the canvas's bottom wall, game over.
 		    // if (ctx.getImageData(ball_x, ball_y + 5 + ball_size/2, 1, 1).data[0] == 119 && ball_dy > 0) {
 		    if (ball_y + (ball_size/2) >= canvas.height && ball_dy < 0) {
