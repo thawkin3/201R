2163c2856a8724253e06704cf0c5c77bc19c8471
Tyler Hawkins <thawkin3@byu.net>
2016-03-20 21:50:49 -0600

    fixing ball location

--- MongoDBreakout/public/javascripts/app/controllers/gameController.js
+++ MongoDBreakout/public/javascripts/app/controllers/gameController.js
@@ -314,7 +314,7 @@
 		    	ball_dy = -ball_dy; 
 		    }
 
-		    if (ball_y >= canvas.height - 10 && ball_y < canvas.height && initialTimer && ball_dy > 0) {
+		    if (ball_y >= canvas.height - 10 && ball_y < canvas.height && initialTimer && ball_dy < 0) {
 				// console.log(ball_y_tracker);
 				console.log("game over");
 			    gameEnd = true;
