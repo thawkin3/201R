140d2dd59d20f549c1352af91037e1b7135e604d
Tyler Hawkins <thawkin3@byu.net>
2016-03-23 23:36:31 -0600

    rest services

--- Blog/models/Comments.js
+++ Blog/models/Comments.js
@@ -4,3 +4,8 @@ var CommentSchema = new mongoose.Schema({
 	upvotes: {type: Number, default: 0},
 });
 mongoose.model('Comment', CommentSchema);
+
+CommentSchema.methods.upvote = function(cb) {
+  this.upvotes += 1;
+  this.save(cb);
+};
--- Blog/routes/index.js
+++ Blog/routes/index.js
@@ -21,6 +21,30 @@ router.post('/comments', function(req, res, next) {
   });
 });
 
+/* Helper Param */
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error("can't find comment")); }
+    req.comment = comment;
+    return next();
+  });
+});
+
+/* GET a single comment */
+router.get('/comments/:comment', function(req, res) {
+  res.json(req.comment);
+});
+
+/* PUT an upvote on a comment */
+router.put('/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment){
+    if (err) { return next(err); }
+    res.json(comment);
+  });
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
