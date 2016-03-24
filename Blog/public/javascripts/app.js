f15fa76bb070242d4f2aeb161ee4d5ae7762298c
Tyler Hawkins <thawkin3@byu.net>
2016-03-24 16:44:30 -0600

    fiing blog put

--- Blog/public/javascripts/app.js
+++ Blog/public/javascripts/app.js
@@ -6,11 +6,11 @@ angular.module('comment', [])
 	    $scope.test = 'Hello world!';
 	    
 	    $scope.comments = [
-			{ title: 'Comment 1', upvotes: 5 },
-			{ title: 'Comment 2', upvotes: 6 },
-			{ title: 'Comment 3', upvotes: 1 },
-			{ title: 'Comment 4', upvotes: 4 },
-			{ title: 'Comment 5', upvotes: 3 }
+			// { title: 'Comment 1', upvotes: 5 },
+			// { title: 'Comment 2', upvotes: 6 },
+			// { title: 'Comment 3', upvotes: 1 },
+			// { title: 'Comment 4', upvotes: 4 },
+			// { title: 'Comment 5', upvotes: 3 }
 	    ];
 
 	    $scope.addComment = function() {
