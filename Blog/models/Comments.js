56d6d271f0548f4392e27decb564d88eb11d1f96
Tyler Hawkins <thawkin3@byu.net>
2016-03-23 23:19:09 -0600

    routes

--- Blog/models/Comments.js
+++ Blog/models/Comments.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var CommentSchema = new mongoose.Schema({
-  title: String,
-  upvotes: {type: Number, default: 0},
+	title: String,
+	upvotes: {type: Number, default: 0},
 });
 mongoose.model('Comment', CommentSchema);
--- Blog/routes/index.js
+++ Blog/routes/index.js
@@ -1,6 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var Comment = mongoose.model('Comment');
+
+/* GET comments */
+router.get('/comments', function(req, res, next) {
+  Comment.find(function(err, comments){
+    if(err){ return next(err); }
+    res.json(comments);
+  });
+});
+
+/* POST a comment */
+router.post('/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+    res.json(comment);
+  });
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
