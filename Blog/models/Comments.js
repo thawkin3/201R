054a68a054e4301a83430a54501536653f1bcfbb
Tyler Hawkins <thawkin3@byu.net>
2016-03-24 16:53:27 -0600

    put comments

--- Blog/models/Comments.js
+++ Blog/models/Comments.js
@@ -3,9 +3,10 @@ var CommentSchema = new mongoose.Schema({
 	title: String,
 	upvotes: {type: Number, default: 0},
 });
-mongoose.model('Comment', CommentSchema);
 
 CommentSchema.methods.upvote = function(cb) {
   this.upvotes += 1;
   this.save(cb);
 };
+
+mongoose.model('Comment', CommentSchema);
