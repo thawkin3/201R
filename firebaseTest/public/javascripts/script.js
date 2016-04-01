c88fb3c6a8657a364d9325ba635371a256464aa2
thawkin3 <thawkin3@byu.net>
2016-04-01 18:06:05 +0000

    output

---
+++ firebaseTest/public/javascripts/script.js
@@ -0,0 +1,13 @@
+var myApp = angular.module("myApp", ["firebase"]);
+myApp.controller("chatController", ["$scope", "$firebaseArray", 
+    function($scope, $firebaseArray) { 
+        var ref = new Firebase("https://brilliant-fire-7841.firebaseio.com/");
+        $scope.chats = $firebaseArray(ref);
+        $scope.update = function(user) { 
+            var newmessage = {from:user.name || "anonymous",body:user.chat};
+            console.log(newmessage);
+            $scope.chats.$add(newmessage);
+            user.chat = ""; 
+        } 
+     } 
+]);
