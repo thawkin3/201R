4e099ffd0b3d8a7ba222248f4f482307efc040f7
Tyler Hawkins <thawkin3@byu.net>
2016-06-15 23:36:56 -0600

    routing for everyting but new and go

--- URLShortener/routes/index.js
+++ URLShortener/routes/index.js
@@ -130,9 +130,4 @@ router.get('/go/:lookupNumber(*)', function(req, res, next) {
 
 });
 
-/* GET home page from all other possible requests. */
-router.get('*', function(req, res, next) {
-  res.redirect(301, '/');
-});
-
 module.exports = router;
