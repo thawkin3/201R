9b13e9c050aab7cf915cb03cb820dda44d16b40d
tylerh <tylerh@qualtrics.com>
2017-05-30 12:56:22 -0600

    Movie Database: cleaning up the JS file to make it more readable

--- Movies/script.js
+++ Movies/script.js
@@ -39,7 +39,19 @@ function runAJAX(theResource) {
 			movieArr.push({"title": title, "rating": rating, "popularity": popularity, "overview": overview, "poster": poster});
 		}
 
-		$("#container").append("<table><thead><tr style = 'background: #6F8DC3;'><th id='poster'>Poster</th><th class='sortable' id='title'>Title<img class='arrow activeSort' src='up.png' /></th><th class='sortable' id='rating'>Rating<img class='arrow' src='up.png' /></th><th class='sortable' id='popularity'>Popularity<img class='arrow' src='up.png' /></th><th id='overview'>Overview</th></tr><tbody></tbody></table>");
+		$("#container").append(
+			"<table>" + 
+				"<thead>" + 
+					"<tr style='background: #6F8DC3;'>" + 
+						"<th id='poster'>Poster</th>" + 
+						"<th class='sortable' id='title'>Title<img class='arrow activeSort' src='up.png' /></th>" + 
+						"<th class='sortable' id='rating'>Rating<img class='arrow' src='up.png' /></th>" + 
+						"<th class='sortable' id='popularity'>Popularity<img class='arrow' src='up.png' /></th>" + 
+						"<th id='overview'>Overview</th>" + 
+					"</tr>" + 
+				"<tbody></tbody>" + 
+			"</table>"
+		);
 	
 		$("th").children().removeClass("activeSort");
 
@@ -64,10 +76,18 @@ function runAJAX(theResource) {
 		movieArr = movieArr.sort(movieSort);
 		
 		for (var i = 0; i < movieArr.length; i++) {
-			$("table tbody").append("<tr><td><img class='pics' src ='" + movieArr[i].poster + "' /></td><td>" + movieArr[i].title + "</td><td>" + movieArr[i].rating + "</td><td>" + movieArr[i].popularity + "</td><td style='max-width:300px'>" + movieArr[i].overview + "</td></tr>");
+			$("table tbody").append(
+				"<tr>" + 
+					"<td><img class='pics' src ='" + movieArr[i].poster + "' /></td>" + 
+					"<td>" + movieArr[i].title + "</td>" + 
+					"<td>" + movieArr[i].rating + "</td>" + 
+					"<td>" + parseFloat(movieArr[i].popularity).toFixed(2) + "</td>" + 
+					"<td style='max-width:300px'>" + movieArr[i].overview + "</td>" + 
+				"</tr>"
+			);
 		}
-		$( "tbody tr:odd" ).css( "background-color", "#8DABE1" );
-		$( "tbody tr:even").css( "background-color", "#93B8FB");
+		$("tbody tr:odd").css("background-color", "#8DABE1");
+		$("tbody tr:even").css("background-color", "#93B8FB");
 
 	}).fail(function(){
 		$("#container").html("Sorry! Something's not working right now. Please check back later.");
@@ -119,9 +139,16 @@ $(document).on("click", "th", function(){
 		movieArr = movieArr.sort(movieSort);
 		$("table tbody").html("");
 		for (var i = 0; i < movieArr.length; i++) {
-			$("table tbody").append("<tr><td><img class='pics' src='" + movieArr[i].poster + "' /></td><td>" + movieArr[i].title + "</td><td>" + movieArr[i].rating + "</td><td>" + (movieArr[i].popularity).toFixed(2) + "</td><td style='max-width:300px'>" + movieArr[i].overview + "</td></tr>");
+			$("table tbody").append(
+				"<tr>" + 
+					"<td><img class='pics' src='" + movieArr[i].poster + "' /></td>" + 
+					"<td>" + movieArr[i].title + "</td><td>" + movieArr[i].rating + "</td>" + 
+					"<td>" + parseFloat(movieArr[i].popularity).toFixed(2) + "</td>" + 
+					"<td style='max-width:300px'>" + movieArr[i].overview + "</td>" + 
+				"</tr>"
+			);
 		}
-		$( "tbody tr:odd" ).css( "background-color", "#8DABE1" );
-		$( "tbody tr:even").css( "background-color", "#93B8FB");
+		$("tbody tr:odd").css("background-color", "#8DABE1");
+		$("tbody tr:even").css("background-color", "#93B8FB");
 	}
 });
