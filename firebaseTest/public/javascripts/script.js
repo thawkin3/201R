48af573a69a8f81d164352dfcb0a0c04675e4d3a
Tyler Hawkins <thawkin3@byu.net>
2017-05-30 22:55:13 -0600

    Firebase chat: changed firebase instance

--- firebaseTest/public/javascripts/script.js
+++ firebaseTest/public/javascripts/script.js
@@ -1,7 +1,8 @@
 var myApp = angular.module("myApp", ["firebase"]);
 myApp.controller("chatController", ["$scope", "$firebaseArray", 
     function($scope, $firebaseArray) { 
-        var ref = new Firebase("https://brilliant-fire-7841.firebaseio.com/");
+        // var ref = new Firebase("https://brilliant-fire-7841.firebaseio.com/");
+        var ref = new Firebase("https://dazzling-inferno-8314.firebaseio.com/");
         $scope.chats = $firebaseArray(ref);
         $scope.update = function(user) { 
             var newmessage = {from:user.name || "anonymous",body:user.chat};
