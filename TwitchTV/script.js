f9a04467eb27b486ff75658a1bf9822a298b4ef4
Tyler Hawkins <thawkin3@byu.net>
2017-05-30 23:36:00 -0600

    Changed location of GrowingBubbles and Squarexploder to their own folders; made Dot Chaser its own file instead of a survey; started on TwitchTV

--- TwitchTV/script.js
+++ TwitchTV/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	// array of channels to use
-	var channels = ["freecodecamp", "TR7K", "ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "brunofin", "comster404"];
+	var channels = ["TSM_Bjergsen", "freecodecamp", "TR7K", "ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "brunofin", "comster404"];
 
 	// build the table
 	channels.forEach(function(element, index, array) {
@@ -16,7 +16,13 @@ $(document).ready(function(){
 				display_name = data.stream.channel.display_name;
 				game = data.stream.channel.game;
 				status = data.stream.channel.status;
-				$("tbody").prepend("<tr class='normal'><td><img class='logo' src='" + logo + "'/></td><td><a target='_blank' href='http://www.twitch.tv/" + display_name + "'>" + display_name + "</a></td><td>" + game + ": " + status + "</td></tr>");
+				$("tbody").prepend(
+					"<tr class='normal'>" + 
+						"<td><img class='logo' src='" + logo + "'/></td>" + 
+						"<td><a target='_blank' href='http://www.twitch.tv/" + display_name + "'>" + display_name + "</a></td>" + 
+						"<td>" + game + ": " + status + "</td>" + 
+					"</tr>"
+				);
 			} else if (data.stream == null) {
 				$.getJSON("https://api.twitch.tv/kraken/channels/" + element, function(data) {
 					logo = data.logo;
@@ -26,7 +32,13 @@ $(document).ready(function(){
 					display_name = data.display_name;
 					status = "Offline";
 					rowShade = "offline";
-					$("tbody").append("<tr class='offline'><td><img class='logo' src='" + logo + "'/></td><td><a target='_blank' href='http://www.twitch.tv/" + display_name + "'>" + display_name + "</a></td><td>" + status + "</td></tr>");
+					$("tbody").append(
+						"<tr class='offline'>" + 
+							"<td><img class='logo' src='" + logo + "'/></td>" + 
+							"<td><a target='_blank' href='http://www.twitch.tv/" + display_name + "'>" + display_name + "</a></td>" + 
+							"<td>" + status + "</td>" + 
+						"</tr>"
+					);
 				});
 			}
 		}).fail(function(){
@@ -35,7 +47,13 @@ $(document).ready(function(){
 			display_name = element;
 			status = "Account Closed";
 			rowShade = "offline";
-			$("tbody").append("<tr class='closed'><td><img class='logo' src='" + logo + "'/></td><td><a target='_blank' href='http://www.twitch.tv/" + display_name + "'>" + display_name + "</a></td><td>" + status + "</td></tr>");
+			$("tbody").append(
+				"<tr class='closed'>" +
+					"<td><img class='logo' src='" + logo + "'/></td>" + 
+					"<td><a target='_blank' href='http://www.twitch.tv/" + display_name + "'>" + display_name + "</a></td>" + 
+					"<td>" + status + "</td>" + 
+				"</tr>"
+			);
 		});
 	});
 
