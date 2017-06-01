4efe5ac5106ae026184d2709747433161168cbed
tylerh <tylerh@qualtrics.com>
2017-06-01 12:59:43 -0600

    Utah Weather Node: moving to its own directory

---
+++ UtahWeatherNode/public/script.js
@@ -0,0 +1,95 @@
+$(document).ready(function () {
+
+	$("#cityfield").keyup(function () {
+        
+        var str = $('#cityfield').val();
+        if (str != "") {
+			str = str.toLowerCase().trim();
+			strArr = str.split(" ");
+			for (var i = 0; i < strArr.length; i++) {
+				strArr[i] = strArr[i][0].toUpperCase() + strArr[i].substr(1);
+			}
+			str = strArr.join(" ");
+		}
+
+        $.getJSON("../getcity?q="+str, function(data) {
+	    	var everything;
+	    	everything = "<ul>";
+	    	$.each(data, function(i, item) {
+	      		everything += "<li> "+data[i].city;
+	    	});
+	    	everything += "</ul>";
+	    	$("#txtHint").html(everything);
+	    })
+	    .done(function() { 
+	    	console.log('getJSON request succeeded!'); 
+	    })
+	    .fail(function(jqXHR, textStatus, errorThrown) { 
+	    	console.log('getJSON request failed!' + textStatus); 
+	    	console.log("incoming " + jqXHR.responseText);
+	    })
+	    .always(function() { 
+	    	console.log('getJSON request ended!');
+	    })
+	    .complete(function() { 
+	    	console.log("complete"); 
+		});
+
+    });
+
+    $("#searchForm").submit(function (e) {
+		
+		var value = $("#cityfield").val();
+		value = value.toLowerCase().trim();
+		if (value != "") {
+			valArr = value.split(" ");
+			for (var i = 0; i < strArr.length; i++) {
+				valArr[i] = valArr[i][0].toUpperCase() + valArr[i].substr(1);
+			}
+			value = valArr.join(" ");
+			$("#dispcity").text(value);
+			e.preventDefault();
+			var theList = $("#txthint li");
+			for (var i = 0; i < theList.length; i++) {
+				if (theList[i].innerHTML.trim() != value) {
+					theList[i].style.display = "none";
+				} else {
+					theList[i].style.fontWeight = "normal";
+				}
+			}
+
+			var myurl= "https://api.wunderground.com/api/2fbe9dd598f2cfba/geolookup/conditions/q/UT/";
+			myurl += value;
+			myurl += ".json";
+			console.log(myurl);
+			
+			$.ajax({
+			  	url : myurl,
+			  	dataType : "jsonp",
+			  	success : function(data) {
+			    	var location = data['location']['city'];
+			    	var temp_string = data['current_observation']['temperature_string'];
+			    	var current_weather = data['current_observation']['weather'];
+			    	var icon = data['current_observation']['icon_url'];
+			    	everything = "<ul>";
+			    	everything += "<li>Location: " + location;
+			    	everything += "<li>Temperature: " + temp_string;
+			    	everything += "<li>Weather: " + current_weather;
+			    	everything += "</ul>";
+			    	everything += "<img src='" + icon + "'/>";
+			    	$("#weather").html(everything);
+		  		}
+			});
+		}
+
+	});
+
+	$(document).on("click", "li", function () {
+		$("li").css("font-weight", "normal");
+		$(this).css("font-weight", "bold")
+		$("#cityfield").val($(this).html().trim());
+		$("#button").focus();
+		$("#searchForm").submit();
+	});
+
+});
---
+++ UtahWeatherNode/routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.sendFile('index.html', { root:  'public' });
+});
+
+router.get('/getcity',function(req, res, next) {
+	console.log("In getcity route");
+	
+	fs.readFile(__dirname + '/cities.txt', function(err, data) {
+        
+        if (err) throw err;
+        
+        var myRe = new RegExp("^" + req.query.q);
+        console.log(myRe);
+        var jsonResult = [];
+        var cities = data.toString().split("\n");
+        
+        for (var i = 0; i < cities.length; i++) {
+        	var result = cities[i].search(myRe);
+          	if (result != -1) {
+            	console.log(cities[i]);
+            	jsonResult.push({city:cities[i]});
+          	}
+        }
+
+        console.log(jsonResult);
+
+        res.status(200).json(jsonResult);
+
+    });
+});
+
+module.exports = router;
