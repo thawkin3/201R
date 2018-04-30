5260b34ca8acf49006ac22b09aa909ba003462d8
Tyler Hawkins <thawkins@youniqueproducts.com>
2018-04-30 12:50:58 -0600

    finished media queries

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-	
 	// Scroll animation
 	$(".nav a, .separator a").click(function() {
 	    $("html, body").animate({
@@ -8,4 +7,13 @@ $(document).ready(function () {
 	    return false;
 	});
 
+	// Wow animations
+	var wow = new WOW({
+		boxClass: 'wow',
+		animateClass: 'animated',
+		offset: 0,
+		mobile: false,
+		live: false
+	});
+	wow.init();
 });
