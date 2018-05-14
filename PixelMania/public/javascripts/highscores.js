35e61cb9aaa6104cea1b73a6b13257d69bf4c202
Tyler Hawkins <thawkins@youniqueproducts.com>
2018-05-14 13:18:07 -0600

    style updates

--- PixelMania/public/javascripts/highscores.js
+++ PixelMania/public/javascripts/highscores.js
@@ -4,11 +4,17 @@ $(document).ready(function(){
 			url: '/gethighscores',
 			type: 'GET',
 			contentType: 'application/json; charset=utf-8',
-			success: function(data,textStatus) {
-  				for (var i = 0; i < data.length; i++) {
-  					$('#highScoresTable tbody').append('<tr><td>' + data[i].Username + '</td><td>' + data[i].Score + '</td></tr>');
-				}
+	})
+		.done(function(data, textStatus) {
+			var html = '';
+			for (var i = 0; i < data.length; i++) {
+				html += '<tr><td>' + data[i].Username + '</td><td>' + data[i].Score + '</td></tr>';
 			}
-	});
+			$('#highScoresTable tbody').append(html);
+		})
+		.fail(function(error) {
+			$('#highScoresTable tbody').empty();
+			$('#highScoresTable').after('<p>Sorry, there was an error fetching high scores.</p>');
+		});
 
 });
--- PixelMania/public/javascripts/login.js
+++ PixelMania/public/javascripts/login.js
@@ -1,10 +1,8 @@
-$(document).ready(function(){
-
-	$("#loginForm").submit(function(){
+$(document).ready(function() {
+	$("#loginForm").submit(function() {
 		var username = $("#username").val().toUpperCase() || "GUEST";
 		$.cookie("username", username);
 		$("#username").val("");
 		window.location.pathname = "/";
 	});
-
 });
