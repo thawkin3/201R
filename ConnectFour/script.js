bb97b3881e9999b7511f8c7675f5b9b57e6e93c3
Tyler Hawkins <thawkins@youniqueproducts.com>
2018-08-23 17:07:55 -0600

    whitespace change

--- ConnectFour/script.js
+++ ConnectFour/script.js
@@ -1,93 +1,93 @@
 $(document).ready(function () {
-	var board = [
-		['o','o','o','o','o','o','o'],
-		['o','o','o','o','o','o','o'],
-		['o','o','o','o','o','o','o'],
-		['o','o','o','o','o','o','o'],
-		['o','o','o','o','o','o','o'],
-		['o','o','o','o','o','o','o'],
-	];
-	var playing = false;
-	
-	$('#start').on('click', startGame);
-	$('.piece').on('click', playPiece);
+    var board = [
+        ['o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o'],
+        ['o','o','o','o','o','o','o'],
+    ];
+    var playing = false;
+    
+    $('#start').on('click', startGame);
+    $('.piece').on('click', playPiece);
 
-	function startGame() {
-		$(this).hide();
-	}
+    function startGame() {
+        $(this).hide();
+    }
 
-	function playPiece() {
-		if (!playing) {
-			playing = true;
-		$('#reset').on('click', resetGame);
-		}
+    function playPiece() {
+        if (!playing) {
+            playing = true;
+            $('#reset').on('click', resetGame);
+        }
 
-		var row = parseInt($(this).attr('id').substr(1,1));
-		var col = parseInt($(this).attr('id').substr(2));
+        var row = parseInt($(this).attr('id').substr(1,1));
+        var col = parseInt($(this).attr('id').substr(2));
 
-		var topValue = 80 + (row * 70);
-		$(this).animate({top: '+=' + topValue + 'px'}).off('click', playPiece);
-		
-		if ($(this).hasClass('p2')) {
-			$(this).addClass('droppedP2');
-			board[row][col] = 'b';
-		} else {
-			$(this).addClass('droppedP1');
-			board[row][col] = 'r';
-		};
-		
-		var horizontal = '', vertical = '', rdiag = '', ldiag = '';
-		for (var i = -6; i < 7; i++) {
-			if (typeof board[row][i] !== 'undefined') {
-				horizontal += board[row][i];
-			}
-			if (typeof board[i] !== 'undefined') {
-				vertical += board[i][col];
-			}
-			if (typeof board[row+i] !== 'undefined' && typeof board[row+i][col-i] !== 'undefined') {
-				rdiag += board[row+i][col-i];
-			}
-			if (typeof board[row+i] !== 'undefined' && typeof board[row+i][col+i] !== 'undefined') {
-				ldiag += board[row+i][col+i];
-			}
-		}
-		var check = horizontal + 'o' + vertical + 'o' + rdiag + 'o' + ldiag;
-		
-		if (check.indexOf('rrrr') > -1) {
-			$('#redWin').show();
-		} else if (check.indexOf('bbbb') > -1) {
-			$('#blackWin').show();
-		} else {
-			$('.piece').toggleClass('p2');
-		}
-	}
+        var topValue = 80 + (row * 70);
+        $(this).animate({top: '+=' + topValue + 'px'}).off('click', playPiece);
+        
+        if ($(this).hasClass('p2')) {
+            $(this).addClass('droppedP2');
+            board[row][col] = 'b';
+        } else {
+            $(this).addClass('droppedP1');
+            board[row][col] = 'r';
+        };
+        
+        var horizontal = '', vertical = '', rdiag = '', ldiag = '';
+        for (var i = -6; i < 7; i++) {
+            if (typeof board[row][i] !== 'undefined') {
+                horizontal += board[row][i];
+            }
+            if (typeof board[i] !== 'undefined') {
+                vertical += board[i][col];
+            }
+            if (typeof board[row+i] !== 'undefined' && typeof board[row+i][col-i] !== 'undefined') {
+                rdiag += board[row+i][col-i];
+            }
+            if (typeof board[row+i] !== 'undefined' && typeof board[row+i][col+i] !== 'undefined') {
+                ldiag += board[row+i][col+i];
+            }
+        }
+        var check = horizontal + 'o' + vertical + 'o' + rdiag + 'o' + ldiag;
+        
+        if (check.indexOf('rrrr') > -1) {
+            $('#redWin').show();
+        } else if (check.indexOf('bbbb') > -1) {
+            $('#blackWin').show();
+        } else {
+            $('.piece').toggleClass('p2');
+        }
+    }
 
-	function resetGame() {
-		playing = false;
-		if ($('#blackWin').get(0).style.display !== 'block' && $('#redWin').get(0).style.display !== 'block') {
-			$('#quitEarly').show();
-		}
-		$('.piece').off('click', playPiece);
-		$('#reset').off('click', resetGame);
-		$('.droppedP1, .droppedP2').animate({top: '+=500px'}, 2000, function () {
-			$('.droppedP1, .droppedP2').hide();
+    function resetGame() {
+        playing = false;
+        if ($('#blackWin').get(0).style.display !== 'block' && $('#redWin').get(0).style.display !== 'block') {
+            $('#quitEarly').show();
+        }
+        $('.piece').off('click', playPiece);
+        $('#reset').off('click', resetGame);
+        $('.droppedP1, .droppedP2').animate({top: '+=500px'}, 2000, function () {
+            $('.droppedP1, .droppedP2').hide();
 
-			board = [
-				['o','o','o','o','o','o','o'],
-				['o','o','o','o','o','o','o'],
-				['o','o','o','o','o','o','o'],
-				['o','o','o','o','o','o','o'],
-				['o','o','o','o','o','o','o'],
-				['o','o','o','o','o','o','o'],
-			];
-			$('.piece').removeClass('droppedP1 droppedP2').toggleClass('p2');
-			$('.piece').css({
-					top: '0px',
-					display: 'block',
-				});
-			$('.piece').off('click', playPiece).on('click', playPiece);
-			$('#blackWin, #redWin, #quitEarly').hide();
-			$('#start').show();
-		});
-	}
+            board = [
+                ['o','o','o','o','o','o','o'],
+                ['o','o','o','o','o','o','o'],
+                ['o','o','o','o','o','o','o'],
+                ['o','o','o','o','o','o','o'],
+                ['o','o','o','o','o','o','o'],
+                ['o','o','o','o','o','o','o'],
+            ];
+            $('.piece').removeClass('droppedP1 droppedP2').toggleClass('p2');
+            $('.piece').css({
+                    top: '0px',
+                    display: 'block',
+                });
+            $('.piece').off('click', playPiece).on('click', playPiece);
+            $('#blackWin, #redWin, #quitEarly').hide();
+            $('#start').show();
+        });
+    }
 });
