c102a376b0ecb173b9268a01f0281064a8e86a6b
Tyler Hawkins <thawkins@instructure.com>
2019-08-23 16:17:24 -0600

    Updated hard-coded ip address in the TimeStamp microservice

--- TimeStamp/public/javascripts/script.js
+++ TimeStamp/public/javascripts/script.js
@@ -1,22 +1,23 @@
 $(document).ready(function(){
+  $("#dateForm").submit(function(){
+    var str = $("#theDate").val();
 
-	$("#dateForm").submit(function(){
+    $.getJSON("../gettime?q="+str)
+      .done(function(data) {
+        $("#unix").html(data.unix);
+        $("#normal").html(data.normal);
+        if ($("#normal").html() == "") {
+          $("#unix").html("Invalid Date");
+          $("#normal").html("Invalid Date");
+        }
+      })
+      .fail(function(jqXHR, textStatus, errorThrown) { 
+      });
+  });
 
-		var str = $("#theDate").val();
-
-		$.getJSON("../gettime?q="+str, function(data) {
-	    	$("#unix").html(data.unix);
-	    	$("#normal").html(data.normal);
-	    	if ($("#normal").html() == "") {
-	    		$("#unix").html("Invalid Date");
-	    		$("#normal").html("Invalid Date");
-	    	}
-	    })
-	    .done(function() { 
-	    })
-	    .fail(function(jqXHR, textStatus, errorThrown) { 
-	    });
-
-	});
-
+  var theSpots = document.getElementsByClassName("currentAddress");
+  for (var i = 0; i < theSpots.length; i++) {
+    theSpots[i].innerHTML = window.location.href;
+  }
 });
+
