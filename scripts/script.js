d93ed29b8d950cfa5aa835a5ab7c7d5636b82980
Tyler Hawkins <tylerhawkins@workfront.com>
2020-07-31 14:49:50 -0600

    formats script file and removes commented out code

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,21 @@
-$(document).ready(function () {
-	// Scroll animation
-	$('.nav a, .separator a').click(function() {
-	    $('html, body').animate({
-	        scrollTop: $($(this).attr('href')).offset().top
-	    }, 500);
-	    return false;
-	});
+$(document).ready(function() {
+  // Scroll animation
+  $('.nav a, .separator a').click(function() {
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 500);
+    return false;
+  });
 
-	// Wow animations
-	// var wow = new WOW({
-	// 	boxClass: 'wow',
-	// 	animateClass: 'animated',
-	// 	offset: 0,
-	// 	mobile: false,
-	// 	live: false
-	// });
-	// wow.init();
+  // Navbar collapse fix for mobile navbar when link is clicked
+  $('.navbar-nav li a').on('click', function(event) {
+    $('.navbar-collapse').collapse('hide');
+  });
 
-	// Navbar collapse fix for mobile navbar when link is clicked
-	$('.navbar-nav li a').on('click', function(event) {
-		$('.navbar-collapse').collapse('hide');
-	});
-
-	// Window resize fix for navbar if currently expanded
-	$(window).on('resize', function(event) {
-		if (window.innerWidth >= 768) {
-			$('.navbar-collapse').collapse('hide');
-		}
-	});
+  // Window resize fix for navbar if currently expanded
+  $(window).on('resize', function(event) {
+    if (window.innerWidth >= 768) {
+      $('.navbar-collapse').collapse('hide');
+    }
+  });
 });
